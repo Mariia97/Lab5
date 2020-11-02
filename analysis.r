52bcae79c77e09ad4ad736d652dec1ceec3ad23e
Mariia97 <60774229+Mariia97@users.noreply.github.com>
2020-11-02 11:37:05 +0200

    Update analysis.r

--- analysis.r
+++ analysis.r
@@ -5,25 +5,28 @@ download.file(Url, destfile=paste0(getwd(), "/UCI HAR Dataset.zip"), method = "c
 unzip("UCI HAR Dataset.zip", exdir = getwd())
 
 # Read and convert data
-## Read data file by file and merge into a single dataset
+### Read data file by file and merge into a single dataset
 features <- read.csv('./UCI HAR Dataset/features.txt', header=FALSE, sep = ' ')
-features <- as.character(features[, 2])
+features <- as.character(features[,2])
 
-### Train dataset
 x_train <- read.table('./UCI HAR Dataset/train/X_train.txt')
-y_train <- read.csv('./UCI HAR Dataset/train/y_train.txt', header = FALSE, sep = ' ')
+names(x_train) <- features
+y_train<- read.csv('./UCI HAR Dataset/train/y_train.txt', header = FALSE, sep = ' ')
+names(y_train) <- c("Activity")
 subject_train <- read.csv('./UCI HAR Dataset/train/subject_train.txt',header = FALSE, sep = ' ')
+names(subject_train)<-c("Subject")
 
-train <- data.frame(x_train, y_train, subject_train)
-names(train) <- c(c('subject', 'activity'), features)
+train <- data.frame(subject_train,  y_train, x_train)
 
-### Test dataset
+
 x_test <- read.table('./UCI HAR Dataset/test/X_test.txt')
+names(x_test) <- features
 y_test <- read.csv('./UCI HAR Dataset/test/y_test.txt', header = FALSE, sep = ' ')
+names(y_test)<-c("Activity")
 subject_test <- read.csv('./UCI HAR Dataset/test/subject_test.txt', header = FALSE, sep = ' ')
+names(subject_test) <- c("Subject")
 
-test <-  data.frame(x_test, y_test, subject_test)
-names(test) <- c(c('subject', 'activity'), features)
+test <-  data.frame(subject_test, y_test, x_test)
 
 
 # 1. Merge the Train and Test datasets
@@ -33,19 +36,29 @@ ds_all <-rbind(train, test)
 # 2. Measurements of the mean and standard deviations for each measurement
 
 mean_std <- grep('mean|std', features)
-ds_sup <- ds_all[, c(1, 2, mean_std +2)]
+ds_sub <- ds_all[, c(1, 2, mean_std+2)]
 
 # 3. Use descriptive activity names to rename activities in the dataset
 
 activity_labels <- read.table('./UCI HAR Dataset/activity_labels.txt', header = FALSE)
+activity_labels <- as.character(activity_labels[,2])
+ds_sub$Activity <- activity_labels[ds_sub$Activity]
 
-train_labels <- read.table("./UCI HAR Dataset/train/y_train.txt")
-test_labels <- read.table("./UCI HAR Dataset/test/y_test.txt")
 
-train_labels <- merge(activity_labels, train_labels,by=c("V1"))
-test_labels <- merge(activity_labels, test_labels,by=c("V1"))
+# 4. Replace the names in the data set with the names from activity labels
+name_new <- names(ds_sub)
+name_new <- gsub("[(][)]", "", name_new)
+name_new <- gsub("^t", "TimeDomain_", name_new)
+name_new <- gsub("^f", "FrequencyDomain_", name_new)
+name_new <- gsub("Acc", "Accelerometer", name_new)
+name_new <- gsub("Gyro", "Gyroscope", name_new)
+name_new <- gsub("Mag", "Magnitude", name_new)
+name_new <- gsub("-mean-", "_Mean_", name_new)
+name_new <- gsub("-std-", "_StandardDeviation_", name_new)
+name_new <- gsub("-", "_", name_new)
+names(ds_sub) <- name_new
 
-labels <-rbind(train_labels, test_labels)
-labels$Activity <-labels$V2
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-ds_labels <-cbind(labels, ds_sub)
+data_tidy <- aggregate(ds_sub[,3:81], by = list(activity = ds_sub$Activity, subject = ds_sub$Subject),FUN = mean)
+write.table(x = data_tidy, file = "data_tidy.txt", row.names = FALSE)
